336a7b41444b3748888fd25d85b5aa80f7a58952
aardvarkpepper <aardvarkpepper@gmail.com>
2023-08-27 16:01:44 -0400

    Update db > schema.sql and seed.sql with updated variable names, new variable, reordered inputs to prevent errors.

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -9,6 +9,14 @@ CREATE DATABASE acorn_dev;
 -- Connect to the db
 \c acorn_dev;
 
+-- Create a table for "achievements"
+CREATE TABLE achievements (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(80) NOT NULL,
+  description VARCHAR(120) NOT NULL,
+  image VARCHAR(120) NOT NULL
+);
+
 -- Create a table for "clients"
 CREATE TABLE clients (
   id SERIAL PRIMARY KEY,
@@ -19,12 +27,16 @@ CREATE TABLE clients (
   profile_picture VARCHAR (120)
 );
 
--- Create a table for "achievements"
-CREATE TABLE achievements (
+-- Create a table for "lessons"
+CREATE TABLE lessons (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(80) NOT NULL,
-  description VARCHAR(120) NOT NULL,
-  image VARCHAR(120) NOT NULL
+  category VARCHAR(80) NOT NULL,
+  title VARCHAR(80) NOT NULL,
+  duration VARCHAR(40) NOT NULL,
+  difficulty VARCHAR(15) NOT NULL,
+  description VARCHAR(400) NOT NULL,
+  materials VARCHAR(80),
+  preview VARCHAR(80)
 );
 
 -- Create a table for "client achievements"
@@ -38,20 +50,8 @@ CREATE TABLE client_achievements (
 CREATE TABLE client_lessons_progress (
   id SERIAL PRIMARY KEY,
   client_id INT REFERENCES clients (id) ON DELETE CASCADE,
-  lesson_id INT REFERENCES lessons (id) ON DELETE CASCADE
+  lesson_id INT REFERENCES lessons (id) ON DELETE CASCADE,
   lesson_completion_status BOOLEAN DEFAULT FALSE
-);
-
--- Create a table for "lessons"
-CREATE TABLE lessons (
-  id SERIAL PRIMARY KEY,
-  category VARCHAR(80) NOT NULL,
-  title VARCHAR(80) NOT NULL,
-  duration VARCHAR(40) NOT NULL,
-  difficulty VARCHAR(15) NOT NULL,
-  description VARCHAR(400) NOT NULL,
-  materials VARCHAR(80),
-  preview VARCHAR(80)
 );
 
 -- Create a table for "lesson sections"
--- src/db/seed.sql
+++ src/db/seed.sql
@@ -8,17 +8,22 @@ VALUES
 ('Completed Elements of Art', 'Awarded on completing Elements of Art lesson', 'none'),           
 ('Completed Principles of Art', 'Awarded on completing Principles of Art lesson', 'none');
 
-INSERT INTO client_achievements
-(client_id, achievement_id)
-VALUES
-(1,1),
-(2,1);
-
 INSERT INTO clients
 (registration_datetime, username, email, password, profile_picture)
 VALUES
 ('2023-08-01', 'Alexia Apple', 'alexia@gmail.com', 'apassword', 'none'),
 ('2023-08-02', 'Bartholomew Bagel', 'barty@gmail.com', 'bpassword', 'none');
+
+INSERT INTO lessons
+(category, title, duration, difficulty, description, materials, preview)
+VALUES
+('Elements of Art', 'Lines', '15 Minutes', 'Very Easy', 'Start with the basics of art by learning about lines', 'Pen or Pencil, Paper', 'None');
+
+INSERT INTO client_achievements
+(client_id, achievement_id)
+VALUES
+(1,1),
+(2,1);
 
 INSERT INTO client_lessons_progress
 (client_id, lesson_id, lesson_completion_status)
@@ -26,13 +31,8 @@ VALUES
 (1, 1, TRUE),
 (2, 1, TRUE);
 
-INSERT INTO lessons
-(category, title, duration, difficulty, description, materials, preview)
-VALUES
-('Elements of Art', 'Lines', '15 Minutes', 'Very Easy', 'Start with the basics of art by learning about lines', 'Pen or Pencil, Paper', 'none');
-
 INSERT INTO lesson_sections
-(lesson_id, title, information_text, interactive_element, correct_feedback, incorrect_feedback, lesson_section_completion)
+(lesson_id, title, information_text, interactive_element, correct_feedback, incorrect_feedback)
 VALUES
 (1, 'Introduction', 'Lines are one of the most basic art elements.  They are the underlying foundation of most art.  The qualities of a line can vary in length, direction, and thickness.', 'none', NULL, NULL),
 (1, 'Converging Lines', 'Converging lines, or lines that point in the same direction, draw your eye in to the point where they meet.', 'ConvergingLines', 'Correct!  The lines of the bridge, bridge shadow, sidewalk, and even the building in the back seem to be directing your attention to the couple walking.', 'Hint: Follow the bridge and sidewalk lines to find the area where they meet.');
